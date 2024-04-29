90d377d640d80c14ce5dc4456d0d2cd2d7f26286
tdatIT <tdat.it2k2@gmail.com>
2024-04-29 22:39:40 +0700

    first commit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/latipe.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Latipe-EC.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/latipe-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'latipe-ec', // Usually your GitHub org/user name.
-  projectName: 'latipe-ec', // Usually your repo name.
+  organizationName: 'Latipe-EC', // Usually your GitHub org/user name.
+  projectName: 'latipe-docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Latipe-EC/latipe-docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Latipe-EC/latipe-docs/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
